ba45bbe606fb59e4614fdcea3a63e71af6e072dc
Ishagi-Yoichi <nikunjkr1752003@gmail.com>
2026-05-03 09:09:52 +0530

    task-tracker

--- go-TaskTracker/internal/controller/task.go
+++ go-TaskTracker/internal/controller/task.go
@@ -71,3 +71,81 @@ func AddTask(description string) model.Task {
 	return newTask
 
 }
+
+func MarkTask(id int, status string) []model.Task {
+	tasks, err := LoadTask()
+	utils.CheckNilError(err)
+
+	for i, task := range tasks {
+		if task.ID == id {
+			switch status {
+			case "inprogress":
+				tasks[i].Status = "in-progress"
+				tasks[i].UpdatedAt = time.Now()
+
+			case "done":
+				tasks[i].Status = "done"
+				tasks[i].UpdatedAt = time.Now()
+
+			default:
+				tasks[i].Status = task.Status
+			}
+
+			err := SaveTask(tasks)
+			utils.CheckNilError(err)
+
+			return tasks
+		}
+	}
+	return []model.Task{}
+}
+
+// This function will take a status string and convert it into status model.
+func ParseTaskStatus(s string) (model.TaskStatus, bool) {
+	switch s {
+	case string(model.StatusTodo):
+		return model.StatusTodo, true
+	case string(model.StatusInProgress):
+		return model.StatusInProgress, true
+	case string(model.StatusDone):
+		return model.StatusDone, true
+	default:
+		return "", false
+	}
+}
+
+func UpdateTask(id int, description string) model.Task {
+	tasks, err := LoadTask()
+	utils.CheckNilError(err)
+
+	for i, task := range tasks {
+		if task.ID == id {
+			tasks[i].Description = description
+			tasks[i].UpdatedAt = time.Now()
+
+			err := SaveTask(tasks)
+			utils.CheckNilError(err)
+
+			return tasks[i]
+		}
+	}
+
+	return model.Task{}
+}
+
+func DeleteTask(id int) ([]model.Task, string) {
+	tasks, err := LoadTask()
+	utils.CheckNilError(err)
+
+	for i, task := range tasks {
+		if task.ID == id {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			err := SaveTask(tasks)
+			utils.CheckNilError(err)
+
+			return tasks, "success"
+		}
+	}
+
+	return tasks, "Task not found"
+}
--- go-TaskTracker/main.go
+++ go-TaskTracker/main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"fmt"
+	"go-TaskTracker/internal/controller"
+	"go-TaskTracker/pkg/utils"
 	"os"
+	"strconv"
 )
 
-type Tasks struct{
+type Tasks struct {
 	status string
-	name string
+	name   string
 }
 
-
-func main(){
+func main() {
 	args := os.Args[1:]
 
 	if len(args) == 0 {
@@ -19,15 +21,76 @@ func main(){
 		return
 	}
 
-	switch args[0]{
-		case "add":
-			if len(args)>2{
-				fmt.Println("Usage: task-cli add 'Task description'")
-				return
-			}
-		    description := args[1]
-			task :=
+	switch args[0] {
+	case "add":
+		if len(args) > 2 {
+			fmt.Println("Usage: task-cli add 'Task description'")
+			return
+		}
+		description := args[1]
+		task := controller.AddTask(description)
+		fmt.Printf("Task added successfully (ID: %v)\n", task.ID)
 
+	case "update":
+		if len(args) < 3 {
+			fmt.Println("Usage: task-cli update <id> 'new description'")
+			return
+		}
+		id, err := strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Println("Invalid ID")
+			return
+		}
+		newDescription := args[2]
+		controller.UpdateTask(id, newDescription)
+		fmt.Printf("Task updated successfully (ID: %v)\n", id)
+		return
+
+	case "delete":
+		if len(args) < 2 {
+			fmt.Println("Usage: task-cli delete <id>")
+			return
+		}
+		id, err := strconv.Atoi(args[1])
+		utils.CheckNilError(err)
+
+		_, msg := controller.DeleteTask(id)
+
+		if msg != "success" {
+			fmt.Printf("Task doesn't exist")
+			return
+		}
+		fmt.Println("task deleted successfully!")
+		return
+
+	case "mark-in-progress":
+		if len(args) > 2 {
+			fmt.Println("Usage: task-cli mark in progress <id>")
+			return
+		}
+		id, err := strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Println("Invalid ID")
+			return
+		}
+		controller.MarkTask(id, "inprogress")
+		fmt.Printf("Task marked as in progress (ID: %v)\n", id)
+
+	case "mark-done":
+		if len(args) > 2 {
+			fmt.Println("Usage: task-cli mark done <id>")
+			return
+		}
+		id, err := strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Println("Invalid ID")
+			return
+		}
+		controller.MarkTask(id, "completed")
+		fmt.Printf("Task marked as completed (ID: %v)\n", id)
+
+	default:
+		fmt.Print("Unkown command :(")
 	}
 
 }
